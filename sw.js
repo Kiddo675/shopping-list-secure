34c14cf4373945b7c8e2988ceeda0de947106b3d
Kiddo675 <mert6161@icloud.com>
2026-02-02 18:00:32 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "shoppinglist-secure-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request)));
+});
